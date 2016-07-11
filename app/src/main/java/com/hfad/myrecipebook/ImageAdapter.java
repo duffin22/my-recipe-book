82dd26d35eac8a02c2ba3ed554e0c50f82663838
Matthew Duffin <matthewtduffin@gmail.com>
2016-07-10 19:44:58 -0700

    More layout play

--- app/src/main/java/com/hfad/myrecipebook/ImageAdapter.java
+++ app/src/main/java/com/hfad/myrecipebook/ImageAdapter.java
@@ -6,8 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by matthewtduffin on 09/07/16.
@@ -37,7 +39,9 @@ public class ImageAdapter extends BaseAdapter {
     public View getView(final int position, View child, ViewGroup parent) {
         Log.d("Postion: " , "" + position);
 
+        FrameLayout frameLayout;
         ImageView imageView;
+        TextView textView;
 
         View v =child;
 
@@ -45,30 +49,45 @@ public class ImageAdapter extends BaseAdapter {
             v = inflater.inflate(R.layout.grid_item, parent, false);
         }
 
-        imageView= (ImageView) v.findViewById(R.id.image);
+        frameLayout = (FrameLayout) v.findViewById(R.id.frame);
 
-        imageView.setImageResource(mThumbIds[position]);
-        imageView.setAlpha(0.5f);
-        return imageView;
+        imageView= (ImageView) v.findViewById(R.id.image);
+        textView= (TextView) v.findViewById(R.id.name);
+
+        imageView.setImageResource(mThumbIds[position][0]);
+        imageView.setAlpha(0.7f);
+        textView.setText(mThumbIds[position][1]);
+
+        if (position%2 ==1) {
+            textView.setTextColor(mContext.getResources().getColor(R.color.white));
+            textView.setBackgroundColor(mContext.getResources().getColor(R.color.softBlack));
+        }
+
+        return frameLayout;
     }
 
     // references to images
-    private Integer[] mThumbIds = {
-            R.drawable.food_tester, R.drawable.burger,
-            R.drawable.donuts, R.drawable.fruit,
-            R.drawable.meatballs, R.drawable.burger,
-            R.drawable.fruit, R.drawable.food_tester,
-            R.drawable.donuts, R.drawable.meatballs,R.drawable.food_tester, R.drawable.burger,
-            R.drawable.fruit, R.drawable.meatballs, R.drawable.burger,
-            R.drawable.fruit, R.drawable.food_tester,
-            R.drawable.donuts, R.drawable.meatballs, R.drawable.burger,
-            R.drawable.donuts, R.drawable.fruit,
-            R.drawable.meatballs, R.drawable.burger,
-            R.drawable.fruit, R.drawable.food_tester,
-            R.drawable.donuts, R.drawable.meatballs,R.drawable.food_tester, R.drawable.burger,
-            R.drawable.donuts, R.drawable.fruit,
-            R.drawable.meatballs, R.drawable.burger,
-            R.drawable.fruit, R.drawable.food_tester,
-            R.drawable.donuts, R.drawable.meatballs,R.drawable.food_tester,
-            R.drawable.donuts, R.drawable.meatballs};
+    private Integer[][] mThumbIds = {
+            {R.drawable.food_tester,R.string.fruit_and_veg},
+            {R.drawable.burger,R.string.burger},
+            {R.drawable.fruit,R.string.fruit},
+            {R.drawable.meatballs,R.string.meatballs},
+            {R.drawable.donuts,R.string.donuts},
+            {R.drawable.food_tester,R.string.fruit_and_veg},
+            {R.drawable.burger,R.string.burger},
+            {R.drawable.fruit,R.string.fruit},
+            {R.drawable.meatballs,R.string.meatballs},
+            {R.drawable.donuts,R.string.donuts},
+            {R.drawable.food_tester,R.string.fruit_and_veg},
+            {R.drawable.burger,R.string.burger},
+            {R.drawable.fruit,R.string.fruit},
+            {R.drawable.meatballs,R.string.meatballs},
+            {R.drawable.donuts,R.string.donuts},
+            {R.drawable.food_tester,R.string.fruit_and_veg},
+            {R.drawable.burger,R.string.burger},
+            {R.drawable.fruit,R.string.fruit},
+            {R.drawable.meatballs,R.string.meatballs},
+            {R.drawable.donuts,R.string.donuts},
+    };
+
 }
