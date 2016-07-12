9df4e1dceb42bf6a093fdfa9bd5d6461cc8e8104
Matthew Duffin <matthewtduffin@gmail.com>
2016-07-11 18:04:05 -0700

    camera button now works

--- app/src/main/java/com/hfad/myrecipebook/AddItemActivity.java
+++ app/src/main/java/com/hfad/myrecipebook/AddItemActivity.java
@@ -1,13 +1,51 @@
 package com.hfad.myrecipebook;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class AddItemActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    ImageView imageAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        imageAdd=(ImageView) findViewById(R.id.imageAdd);
+
+        Log.d("Pointer","Entered onCreat method");
+
+        final ImageView cameraButton= (ImageView) findViewById(R.id.cameraButton);
+
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Pointer","Entered onClick creator for camera button");
+                openCameraActivity(view);
+            }
+        });
+    }
+
+
+
+    public void openCameraActivity(View view) {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        Log.d("Pointer","Entered Camera Activity *******");
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageAdd.setImageBitmap(photo);
+        }
     }
 }
